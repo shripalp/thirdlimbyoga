1abe53e84057d0a8baf31e0d259ca57aea00abaf
Shripal <shripalp@gmail.com>
2026-01-14 14:49:23 -0700

    api debud fixed

--- src/app/api/debug/env/route.js
+++ src/app/api/debug/env/route.js
@@ -1,4 +1,9 @@
-return NextResponse.json({
+import { NextResponse } from "next/server";
+
+export const runtime = "nodejs";
+
+export async function GET() {
+  return NextResponse.json({
   hasStripeSecret: !!process.env.STRIPE_SECRET_KEY,
   hasPriceId: !!process.env.STRIPE_PRICE_ID,
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || null,
@@ -6,3 +11,4 @@ return NextResponse.json({
   testEnvWorks: process.env.TEST_ENV_WORKS || null,
 });
 
+}
