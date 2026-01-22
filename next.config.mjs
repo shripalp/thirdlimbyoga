7e8619095d7c9ba50a5f9cd7606b2af027d71952
Shripal <shripalp@gmail.com>
2026-01-21 22:24:42 -0700

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+  // 🔧 Prevent Netlify build from failing on TS / ESLint
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
